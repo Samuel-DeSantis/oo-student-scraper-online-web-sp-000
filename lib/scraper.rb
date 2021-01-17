b2cecf09aeff75bea047626e74b874c79494aa92
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-01-17 22:20:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
-    links = profile_page.css(".social-icon-container").children.css("a").map { |x| x.attribute('href') }
+    links = profile_page.css(".social-icon-container").children.css("a").map { |x| x.attribute('href').value }
     links.each do |link|
       if link.include?("linkedin")
         student[:linkedin] = link
